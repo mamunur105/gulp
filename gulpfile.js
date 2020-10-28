5fde4e30ef3397705e06008c41f66c2fa02d902e
mamunur105 <rmamunur105@gmail.com>
2020-10-29 02:07:35 +0600

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,90 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var imagemin = require('gulp-imagemin');
+var changed = require('gulp-changed');
+var htmlReplace = require('gulp-html-replace');
+var htmlMin = require('gulp-htmlmin');
+var del = require('del');
+var sequence = require('run-sequence');
+
+var config = {
+  dist: 'dist/',
+  src: './',
+  cssin: 'assets/css/**/*.css',
+  jsin: 'assets/js/src/',
+  imgin: 'assets/images/src/**/*.{jpg,jpeg,png,gif}',
+  htmlin: '*.html',
+  scssin: 'assets/scss/**/*.scss',
+  cssout: 'assets/css/',
+  jsout: 'assets/js/',
+  imgout: 'assets/images/',
+  htmlout: 'dist/',
+  scssout: 'assets/css/',
+  cssoutname: 'style.css',
+  jsoutname: 'script.js',
+  cssreplaceout: 'css/style.css',
+  jsreplaceout: 'js/script.js'
+};
+
+const { series, parallel } = require('gulp');
+
+function clean(cb) {
+  // body omitted
+  cb();
+}
+
+function cssTranspile(cb) {
+  // body omitted
+  cb();
+}
+
+function cssMinify(cb) {
+  // body omitted
+  cb();
+}
+
+function jsTranspile(cb) {
+  // body omitted
+  cb();
+}
+
+function jsBundle(cb) {
+  // body omitted
+  cb();
+}
+
+function jsMinify(cb) {
+  // body omitted
+  cb();
+}
+
+function publish(cb) {
+  // body omitted
+  cb();
+}
+
+function build(cb) {
+  // body omitted
+  cb();
+}
+
+
+exports.build = series(
+  clean,
+  parallel(
+    cssTranspile,
+    series(jsTranspile, jsBundle)
+  ),
+  parallel(cssMinify, jsMinify),
+  publish
+);
+
+exports.default = series(clean, build);
+
+// gulp.task('default', ['serve']);
---
+++ readme.md
@@ -0,0 +1,6 @@
+#HTML Boilerplate
+##Installation
+* cd into the project folder
+* type in command `npm install`
+* `npm install gulp -g`
+* then `gulp serve`
